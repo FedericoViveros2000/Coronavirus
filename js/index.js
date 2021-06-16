ecf721214814a8abab64a3b848104a33139329cc
Josfe <josevivers2000@gmail.com>
2021-06-16 11:54:57 -0400

    Actualizacion 2 16/06/2021 11:55AM

--- js/index.js
+++ js/index.js
@@ -1,7 +1,41 @@
 const $btnMoon = document.getElementById('mode');
 
-$btnMoon.addEventListener('click', e =>{
+$btnMoon.addEventListener('click', e => {
 
     document.body.classList.toggle('dark');
+    
+    /* 
+        TODO Guardando el modo Oscuro en LocalStorage
+    */
+
+    if (document.body.classList.contains('dark')) {
+
+        localStorage.setItem('dark-mode', 'true');
+        
+    }else{
+
+        localStorage.setItem('dark-mode', 'false');
+   
+
+    }
 
 })
+
+/* Para comprobar el modo actual, para el tema */
+
+if (localStorage.getItem('dark-mode') === 'true') {
+
+    document.body.classList.add('dark');
+
+}else{   
+                                                                                                
+    document.body.classList.remove('dark');
+
+}
+
+
+/* document.addEventListener('scroll', e => {
+
+    console.log(scr);
+    
+}) */
